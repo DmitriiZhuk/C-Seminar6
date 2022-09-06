7af903b0d887dfd7281d138853d1a2c8cd17965f
Dmitrii Zhuk <dmitry.v.zhuk@gmail.com>
2022-09-06 22:05:37 +0300

    Task 03 complete

---
+++ Task03/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+
+*/
+
+
+//1 Создаю массив
+Console.Write("Введите размер массива: ");
+int arrNum = Convert.ToInt32(Console.ReadLine());
+int[] numbersArray = new int[arrNum];
+
+// 2 Заполняю массив случайными числами
+Random rnd = new Random();
+for (int i = 0; i < arrNum; i++)
+{
+    numbersArray[i] = rnd.Next(1, 100);
+}
+Console.WriteLine("Содержимое  массива = " + string.Join(" ", numbersArray));
+
+// 3 Считаю сумму произведений пар чисел
+int pairSumProizved = 0;
+for (int j = 0; j < (numbersArray.Length / 2); j++)
+{
+    pairSumProizved = pairSumProizved + numbersArray[j] * numbersArray[numbersArray.Length - (j + 1)];
+}
+Console.WriteLine("Сумма произведений пар чисел = " + pairSumProizved);
