df66b8ecbf2a45a270ca45de189539d9b8d77058
Dmitrii Zhuk <dmitry.v.zhuk@gmail.com>
2022-09-06 21:56:19 +0300

    Task 01 complete

---
+++ Task01/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 1: Программа запрашивает натуральное число M, пользователь вводит с 
+клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 4
+*/
+
+Console.Write("Введите числа через пробел: ");
+string[] numArray = Console.ReadLine().Split(" ");
+
+Console.WriteLine(String.Join(" , ", numArray));
+
+int counter = 0;
+for (int i = 0; i < numArray.Length; i++)
+{
+
+    if (Convert.ToInt32(numArray[i]) > 0) { counter++; }
+}
+Console.WriteLine();
+Console.WriteLine("Чисел больше 0: " + counter);
